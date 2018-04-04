1d1e6d314df38be9dd17c9b69ab38cb8e6b47df4
Jerry Li <xiaoroyg@163.com>
2018-04-04 17:44:58 +0800

    Learend map and range.

--- src/main/groovy/com/learn/groovyinaction/chap2/Glance.groovy
+++ src/main/groovy/com/learn/groovyinaction/chap2/Glance.groovy
@@ -8,3 +8,18 @@ def roman = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
 assert roman[4] == 'IV'
 roman[8] = 'VIII'
 assert roman.size() == 9
+
+def http = [
+        100 : 'CONTINUE',
+        200 : 'OK',
+        400 : 'BDA REQUEST'
+]
+assert http[200] == 'OK'
+http[500] = 'INTERNAL SERVER ERROR'
+assert http.size() == 4
+
+def numberRange = 1..10
+assert numberRange.contains(2)
+assert numberRange.size() == 10
+assert numberRange.from == 1
+assert numberRange.to == 10
