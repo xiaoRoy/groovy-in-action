9053d4317226cf54921d21c45b4e22b3a6224cde
Jerry Li <xiaoroyg@163.com>
2018-05-06 12:12:35 +0800

    Learned reactiong on the parameter count or type.

--- src/main/groovy/com/learn/groovyinaction/chap5/UsingClosure.groovy
+++ src/main/groovy/com/learn/groovyinaction/chap5/UsingClosure.groovy
@@ -13,8 +13,31 @@ def static benchmark (int repeat, Closure worker) {
 
 def slow = benchmark(10000) { (int) it / 2 }
 def fast = benchmark(10000) { int number -> number.intdiv(2) }
-assert fast * 2 < slow
+assert fast < slow
 
 def adderB = { one, another = 3 -> one + another }
 assert adderB(1) == 4
 assert adderB.call(1, 5) == 6
+
+
+def static numOfParams (Closure closure) {
+    closure.maximumNumberOfParameters
+}
+
+assert numOfParams(adder) == 2
+assert numOfParams { one -> } == 1
+
+def static paramType (Closure closure) {
+    closure.getParameterTypes()
+}
+
+assert paramType { String text -> } == [String]
+assert paramType { Number number, String text -> } == [Number, String]
+
+def mult = { one, another -> one * another }
+def twoTimes = mult.curry(2)
+assert twoTimes(5) == 10
+def twoTimesB = { another -> mult(2, another) }
+assert twoTimesB(6) == 12
+def twoTimesC = { another -> mult 2, another}
+assert twoTimesC.call(7) == 14
