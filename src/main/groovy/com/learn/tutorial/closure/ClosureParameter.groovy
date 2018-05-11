a5893267bcb2d008cf1d2729264532840fca685a
Jerry Li <xiaoroyg@163.com>
2018-05-11 23:21:09 +0800

    Learned varargs

--- src/main/groovy/com/learn/tutorial/closure/ClosureParameter.groovy
+++ src/main/groovy/com/learn/tutorial/closure/ClosureParameter.groovy
@@ -6,3 +6,13 @@ assert addition('1', '1') == '11'
 
 def greeting = { "Hello, $it" }
 assert greeting("Smith") == 'Hello, Smith'
+
+def emptyParameter = { -> 'Hello, Groovy'}
+assert emptyParameter() == 'Hello, Groovy'
+
+def concatA = { String... args -> args.join('')}
+assert concatA('abc', 'def') == 'abcdef'
+def concatB = { String[] args -> args.join('') }
+assert concatB('abc', 'def') == 'abcdef'
+def multiConcat = { int multiple, String... args -> args.join('') * multiple }
+assert multiConcat(2, 'abc', 'def') == 'abcdefabcdef'
