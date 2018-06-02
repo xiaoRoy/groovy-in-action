d1c685bd69e129a154b1efbaa675e6c08a4bdc80
Jerry Li <xiaoroyg@163.com>
2018-06-02 17:23:15 +0800

    Leaerned switch.

---
+++ src/main/groovy/com/learn/groovyinaction/chap6/ConditionalExecution.groovy
@@ -0,0 +1,20 @@
+package com.learn.groovyinaction.chap6
+
+def argument = "given"
+def standard = "default"
+def result = argument ? argument : standard
+assert result == "given"
+
+def anotherResult = argument ?: standard
+assert anotherResult == "given"
+
+switch (10) {
+    case 0: assert false; break
+    case 0..9: assert false; break
+    case [8, 9, 11]: assert false; break
+    case Float: assert false; break
+    case { number -> number % 3 == 0 }: assert false; break
+    case { it % 3 == 0 }: assert false; break
+    case ~/../: assert true; break
+    default: assert false; break
+}
