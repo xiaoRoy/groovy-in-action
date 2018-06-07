7f86456fc91b569314d49b536a731a728ab62280
Jerry Li <xiaoroyg@163.com>
2018-06-07 22:57:32 +0800

    Learned  ?. operator.

--- src/main/groovy/com/learn/groovyinaction/chap7/FieldsAndVariables.groovy
+++ src/main/groovy/com/learn/groovyinaction/chap7/FieldsAndVariables.groovy
@@ -5,7 +5,7 @@ import com.learn.groovyinaction.chap7.FieldShowcase
 def scriptLocalVar = 1
 boundVar = 2
 
-def scriptMethod(){
+def scriptMethod () {
     def scriptMethodLocalVar = 1
     scriptMehotBoundvar = 2
 }
@@ -23,3 +23,16 @@ assert pretender.isNoField == 'pretend value'
 assert pretender.count == 0
 pretender.isNoFieldNeither = 'just to increase counter'
 assert pretender.count == 1
+
+def map = [a: [b: [c: 1]]]
+assert map.a.b.c == 1
+if(map && map.a && map.a.x){
+    assert map.a.x.c == null
+}
+try{
+    assert map.a.x.c == null
+}catch (NullPointerException exception){
+    print exception.message
+}
+
+assert map?.a?.x?.c == null
