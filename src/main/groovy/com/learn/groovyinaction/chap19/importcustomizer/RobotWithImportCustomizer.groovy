327b47af16f1e916986aa5a08608cb5097dc8c5c
Jerry Li <xiaoroyg@163.com>
2018-06-17 23:05:15 +0800

    Learned import customizer

---
+++ src/main/groovy/com/learn/groovyinaction/chap19/importcustomizer/RobotWithImportCustomizer.groovy
@@ -0,0 +1,19 @@
+package com.learn.groovyinaction.chap19.importcustomizer
+
+import com.learn.groovyinaction.chap19.Robot
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.ImportCustomizer
+
+def binding = new Binding(robot: new Robot())
+
+def importCustomizer = new ImportCustomizer()
+importCustomizer.addStaticStars 'com.learn.groovyinaction.chap19.Direction'
+
+def config = new CompilerConfiguration()
+config.addCompilationCustomizers importCustomizer
+
+def shell = new GroovyShell(this.class.classLoader, binding, config)
+def script =  """
+    robot.move left
+"""
+shell.evaluate(script)
