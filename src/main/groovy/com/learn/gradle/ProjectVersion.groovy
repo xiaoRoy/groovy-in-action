98e4f352248a760daf4ec2cd98c6e7a95dc35476
Jerry Li <xiaoroyg@163.com>
2018-07-22 22:58:59 +0800

    Added ProjectVersion

---
+++ src/main/groovy/com/learn/gradle/ProjectVersion.groovy
@@ -0,0 +1,12 @@
+package com.learn.gradle
+
+class ProjectVersion {
+
+    Integer major
+    Integer minor
+
+    ProjectVersion (Integer major, Integer minor) {
+        this.major = major
+        this.minor = minor
+    }
+}
