b369d44ba57c278a888e5c74d70af7996d7715e2
Jerry Li <xiaoroyg@163.com>
2018-07-29 16:49:54 +0800

    Learned closure in gradle

--- src/main/groovy/com/learn/gradle/Gradle.groovy
+++ src/main/groovy/com/learn/gradle/Gradle.groovy
@@ -1,14 +1,20 @@
 package com.learn.gradle
 
-def initProjectVersion(major, minor){
+def initProjectVersion (major, minor) {
     new ProjectVersion(major, minor)
 }
 
 assert initProjectVersion(2, 1).class == ProjectVersion.class
 
 def incrementMajorProjectVersion = {
-    it.major ++
+    it.major++
 }
 def projectVersion = initProjectVersion(1, 10)
 incrementMajorProjectVersion(projectVersion)
 assert projectVersion.major == 2
+
+
+def incrementMajorProjectVersionExplicit = { ProjectVersion version -> version.major++ }
+def projectVersionB = initProjectVersion(1, 10)
+incrementMajorProjectVersionExplicit(projectVersionB)
+assert projectVersionB.major == 2
